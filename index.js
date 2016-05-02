c00779aa4cf12c1cb07abb1517c2429a4088e349
Sajjad Anwar <me@sajjad.in>
2016-05-02 16:03:59 +0530

    first cut

---
+++ index.js
@@ -0,0 +1,80 @@
+#!/usr/bin/env node
+
+var overpass = require('query-overpass');
+var fs = require('fs');
+var argv = require('minimist')(process.argv.slice(2));
+var upload = require('mapbox-upload');
+var raju = require('raju');
+
+if (!argv.input || !argv.accessToken || !argv.id) {
+    usage();
+    process.exit(1);
+}
+
+var input = argv.query;
+var accessToken = argv.MapboxAccessToken;
+var mapid = argv.mapid;
+var user = mapid.split('.')[0];
+var id = mapid.split('.')[1];
+
+var query = fs.readFileSync(__dirname + '/' + input, {encoding: 'utf-8'});
+process.stdout.write('Querying overpass...' + '\n');
+overpass(query, function(err, data) {
+    if (err) throw err;
+
+    process.stdout.write('Cleaning up geojson...' + '\n');
+    data.features.forEach(function(feature) {
+        var props = feature.properties;
+        var keys = Object.keys(props);
+        keys.forEach(function(key) {
+            if (props[key] instanceof Array) {
+                props[key].forEach(function(prop) {
+                    flatten(prop, key, props);
+                });
+            }
+        });
+    });
+    var geojson = JSON.stringify(raju(data));
+    console.log(geojson);
+    fs.writeFileSync('query.geojson', geojson);
+    process.stdout.write('Uploading to Mapbox...' + '\n');
+    uploadData(__dirname + '/query.geojson', accessToken, mapid, user);
+});
+
+
+function uploadData(file, accessToken, mapid, user) {
+    var progress = upload({
+        file: file,
+        account: user,
+        accesstoken: accessToken,
+        mapid: mapid 
+    });
+
+    progress.on('error', function(err){
+        if (err) throw err;
+    });
+
+    progress.on('progress', function(p){
+        process.stdout.write(String(Math.floor(p.percentage)) + '%\n') ;
+    });
+
+    progress.once('finished', function(){
+        process.stdout.write('Done! \n');
+    });
+}
+
+function flatten(obj, parentKey, properties) {
+    var objectLength = obj.length;
+    Object.keys(obj).forEach(function(key) {
+        if (obj[key] instanceof Object) {
+            flatten(obj[key], parentKey + '_' + key, properties);
+            delete properties[parentKey];
+        }
+        properties[parentKey + '_' + key] = obj[key];
+        delete properties[parentKey];
+    });
+}
+
+function usage() {
+    process.stdout.write('shaktiman --query path_to_overpass_query \n --MapboxAccessToken \n --mapid \n');
+}
