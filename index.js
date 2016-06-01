d8c6161edd2d2f273bf9f7c39429798b99aa631b
Sajjad Anwar <me@sajjad.in>
2016-06-01 13:36:10 +0530

    upload only if accesstoken and mapid are present

--- index.js
+++ index.js
@@ -5,18 +5,25 @@ var fs = require('fs');
 var argv = require('minimist')(process.argv.slice(2));
 var upload = require('mapbox-upload');
 var raju = require('raju');
+var createTileset = true;
 
-if (!argv.query || !argv.MapboxAccessToken || !argv.mapid) {
+if (!argv.query) {
     usage();
     process.exit(1);
 }
 
+if (!argv.MapboxAccessToken || !argv.mapid) {
+    createTileset = false;
+    process.stdout.write('No access token or mapid specified, will not create tileset.' + '\n');
+} else {
+    var accessToken = argv.MapboxAccessToken;
+    var mapid = argv.mapid;
+    var user = mapid.split('.')[0];
+    var id = mapid.split('.')[1];
+    var name = argv.name;
+}
+
 var input = argv.query;
-var accessToken = argv.MapboxAccessToken;
-var mapid = argv.mapid;
-var user = mapid.split('.')[0];
-var id = mapid.split('.')[1];
-var name = argv.name;
 var overpassUrl = argv.overpassUrl;
 
 var query = fs.readFileSync(__dirname + '/' + input, {encoding: 'utf-8'});
@@ -41,9 +48,12 @@ overpass(query, function(err, data) {
         });
     });
     var geojson = JSON.stringify(raju(data));
+    process.stdout.write('Writing to query.json...' + '\n');
     fs.writeFileSync('query.geojson', geojson);
-    process.stdout.write('Uploading to Mapbox...' + '\n');
-    uploadData(__dirname + '/query.geojson', accessToken, mapid, user);
+    if (createTileset) {
+        process.stdout.write('Uploading to Mapbox...' + '\n');
+        uploadData(__dirname + '/query.geojson', accessToken, mapid, user);
+    }
 }, overpassUrl);
 
 
